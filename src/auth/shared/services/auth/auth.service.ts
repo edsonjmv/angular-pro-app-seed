d44fa54aff433175869f23c8bfcc9329c3ca49be
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2017-12-26 17:00:55 +0100

    Reactive store and AngularFire Observables

--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,13 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { AngularFireAuth } from 'angularfire2/auth';
+import { Store } from "store";
+
+import "rxjs/add/operator/do";
+
+import { AngularFireAuth } from "angularfire2/auth";
+
+export interface User {
+  email: string;
+  uid: string;
+  authenticated: boolean;
+}
 
 @Injectable()
 export class AuthService {
+  auth$ = this.af.authState.do(next => {
+    if (!next) {
+      this.store.set("user", null);
+      return;
+    }
+    const user: User = {
+      email: next.email,
+      uid: next.uid,
+      authenticated: true
+    };
+    this.store.set("user", user);
+  });
 
-  constructor(
-    private af: AngularFireAuth
-  ) {}
+  constructor(private store: Store, private af: AngularFireAuth) {}
 
   createUser(email: string, password: string) {
     return this.af.auth.createUserWithEmailAndPassword(email, password);
@@ -16,5 +36,4 @@ export class AuthService {
   loginUser(email: string, password: string) {
     return this.af.auth.signInWithEmailAndPassword(email, password);
   }
-
 }
