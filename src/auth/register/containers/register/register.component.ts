084ecffc13470582fdb6e238df4337459b6c81ed
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2017-12-26 15:49:21 +0100

    Login/Signup reactive form components

--- src/auth/register/containers/register/register.component.ts
+++ src/auth/register/containers/register/register.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'register',
+  selector: "register",
   template: `
     <div>
-      Register
+      <auth-form (submitted)='registerUser($event)'>
+        <h1>Register</h1>
+        <a routerLink='/auth/login'>Already have an account?</a>
+        <button type='submit'>
+          Create account
+        </button>
+      </auth-form>
     </div>
   `
 })
 export class RegisterComponent {
   constructor() {}
+
+  registerUser(event: FormGroup) {
+    console.log(event.value);
+  }
+
 }
