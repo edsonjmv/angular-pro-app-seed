b433b289441fed3f4b1c1171b882e3cb8d62b7a3
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2017-12-27 15:59:16 +0100

    Implementing AuthGuards

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { AuthGuard } from "../auth/shared/guards/auth.guard";
+
 export const ROUTES: Routes = [
   {
     path: "schedule",
+    canActivate: [AuthGuard],
     loadChildren: "./schedule/schedule.module#ScheduleModule"
   },
-  { path: "meals", loadChildren: "./meals/meals.module#MealsModule" },
+  {
+    path: "meals",
+    canActivate: [AuthGuard],
+    loadChildren: "./meals/meals.module#MealsModule"
+  },
   {
     path: "workouts",
+    canActivate: [AuthGuard],
     loadChildren: "./workouts/workouts.module#WorkoutsModule"
   }
 ];
