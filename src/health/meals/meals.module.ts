6d9ce86b3b80e68b0cbfbd3bbd7aaa15136cc6c6
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2018-01-04 12:13:53 +0100

    Data layer, initiate Observable streams

--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
 
+// shared modules
+import { SharedModule } from "../shared/shared.module";
+
 // containers
 import { MealsComponent } from "./containers/meals/meals.component";
 
 export const ROUTES: Routes = [{ path: "", component: MealsComponent }];
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(ROUTES)],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(ROUTES),
+    SharedModule
+  ],
   declarations: [MealsComponent]
 })
 export class MealsModule {}
