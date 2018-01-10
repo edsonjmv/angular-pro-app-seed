72c646d3e29fae208e9da7fa65e89ee2d1e9afc2
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2018-01-10 15:12:28 +0100

    Schedule Calendar, Observables, BehaviourSubject

--- src/health/schedule/containers/schedule/schedule.component.ts
+++ src/health/schedule/containers/schedule/schedule.component.ts
@@ -1,14 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+
+import { Store } from "store";
+
+import { Observable } from "rxjs/Observable";
+import { Subscription } from "rxjs/Subscription";
+
+import { ScheduleService } from "../../../shared/services/schedule/schedule.service";
 
 @Component({
   selector: "schedule",
   styleUrls: ["schedule.component.scss"],
   template: `
-    <div>
-      Schedule
+    <div class='schedule'>
+
+      <schedule-calendar
+        [date]='date$ | async'>
+      </schedule-calendar>
+
     </div>
   `
 })
-export class ScheduleComponent {
-  constructor() {}
+export class ScheduleComponent implements OnInit, OnDestroy {
+  date$: Observable<Date>;
+  subscription: Subscription[];
+
+  constructor(private store: Store, private scheduleService: ScheduleService) {}
+
+  ngOnInit() {
+    this.date$ = this.store.select("date");
+    this.subscription = [this.scheduleService.schedule$.subscribe()];
+  }
+
+  ngOnDestroy() {
+    this.subscription.forEach(sub => sub.unsubscribe());
+  }
 }
