1f59356c6e38a82434f3daa0df49d27156bd1b2e
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2018-01-10 17:50:41 +0100

    Calendar date toggling

--- src/health/schedule/components/schedule-calendar/schedule-calendar.component.ts
+++ src/health/schedule/components/schedule-calendar/schedule-calendar.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  OnChanges,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
 
 @Component({
   selector: "schedule-calendar",
@@ -7,34 +13,58 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     <div class="calendar">
 
       <schedule-controls
-        [selected]='selectedDay'
-        (move)='onChange($event)'>
+        [selected]="selectedDay"
+        (move)="onChange($event)">
       </schedule-controls>
-
+      <schedule-days
+        [selected]="selectedDayIndex"
+        (select)="selectDay($event)">
+      </schedule-days>
     </div>
   `
 })
-export class ScheduleCalendarComponent {
-
+export class ScheduleCalendarComponent implements OnChanges {
+  selectedDayIndex: number;
   selectedDay: Date;
+  selectedWeek: Date;
 
   @Input()
   set date(date: Date) {
     this.selectedDay = new Date(date.getTime());
   }
 
-  @Output()
-  change = new EventEmitter<Date>();
+  @Output() change = new EventEmitter<Date>();
 
   constructor() {}
 
+  ngOnChanges() {
+    this.selectedDayIndex = this.getToday(this.selectedDay);
+    this.selectedWeek = this.getStartOfWeek(new Date(this.selectedDay));
+  }
+
+  selectDay(index: number) {
+    const selectedDay = new Date(this.selectedWeek);
+    selectedDay.setDate(selectedDay.getDate() + index);
+    this.change.emit(selectedDay);
+  }
+
   onChange(weekOffset: number) {
     const startOfWeek = this.getStartOfWeek(new Date());
-    const startDate = (
-      new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate())
+    const startDate = new Date(
+      startOfWeek.getFullYear(),
+      startOfWeek.getMonth(),
+      startOfWeek.getDate()
     );
-    startDate.setDate(startDate.getDate() + (weekOffset * 7));
+    startDate.setDate(startDate.getDate() + weekOffset * 7);
     this.change.emit(startDate);
+  }
+
+  private getToday(date: Date) {
+    let today = date.getDay() - 1;
+    if (today < 0) {
+      today = 6;
+    }
+    return today;
   }
 
   private getStartOfWeek(date: Date) {
@@ -42,5 +72,4 @@ export class ScheduleCalendarComponent {
     const diff = date.getDate() - day + (day === 0 ? -6 : 1);
     return new Date(date.setDate(diff));
   }
-
 }
